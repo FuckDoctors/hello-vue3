6eb190b824b28c6ce6f50bfcfc68e65e8c66ae95
FuckDoctors <zhbchwin@163.com>
2022-02-26 12:37:34 +0800

    build: 配置cz

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,5 @@
+// https://commitlint.js.org/#/reference-prompt
 module.exports = {
+  // parserPreset: 'conventional-changelog-conventionalcommits',
   extends: ['@commitlint/config-conventional'],
 }
